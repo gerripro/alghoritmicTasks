ded6b762ba8ebfe6c62ddf52d226b590bda5ec26
gerripro <74319105+gerripro@users.noreply.github.com>
2022-07-21 12:25:41 +0400

    Add files via upload

--- romanToInt.java
+++ romanToInt.java
@@ -1,27 +1,27 @@
 class Solution {
-    Map<String, Integer> romanMap  = new HashMap<String, Integer>(){{
-        put("I", 1);
-        put("V", 5);
-        put("X", 10);
-        put("L", 50);
-        put("C", 100);
-        put("D", 500);
-        put("M", 1000);
+    Map<Character, Integer> romanMap  = new HashMap<Character, Integer>(){{
+        put('I', 1);
+        put('V', 5);
+        put('X', 10);
+        put('L', 50);
+        put('C', 100);
+        put('D', 500);
+        put('M', 1000);
     }};
     // 1249 = MCCXLIX
     public int romanToInt(String s) {
+        int len = s.length();
         int totalSum = 0;
         int currentNumber = 0;
         int lastNumber = 0;
-        for (int i=0; i<s.length(); i++) {
-            String charac = Character.toString(s.charAt (i));
-            currentNumber = romanMap.get(charac);
+        for (int i=0; i<len; i++) {
+            currentNumber = romanMap.get(s.charAt (i));
             if (lastNumber < currentNumber) {
                 totalSum = totalSum-lastNumber + (currentNumber-lastNumber);
             } else {
                 totalSum+=currentNumber;
             }
-            lastNumber = romanMap.get(charac);
+            lastNumber = romanMap.get(s.charAt (i));
         }
         return totalSum;
     }
